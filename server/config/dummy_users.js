05a946f18022775f44a3d9ee920e2dc51388741e
mariarojo <mariatrojo@gmail.com>
2018-05-29 14:23:27 -0700

    initial commit from Maria

---
+++ server/config/dummy_users.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    email: 'omar.ihmoda@gmail.com',
+    password: 'Dojo2017',
+    admin: 10,
+  },
+  {
+    email: 'roger@roger.org',
+    password: 'testing1233',
+    admin: 8,
+  },
+  {
+    email: 'mark@slaysman.net',
+    password: 'Testing234535324',
+    admin: 7,
+  },
+];
+
+module.exports = users;
